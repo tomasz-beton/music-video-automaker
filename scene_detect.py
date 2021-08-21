bfbe7746b403a29c3573055f1c75a242cafd99e7
Adam <adam66ak47@gmail.com>
2021-08-21 12:08:12 +0200

    splitting scenes

--- scene_detect.py
+++ scene_detect.py
@@ -1,17 +1,34 @@
 import ffmpeg
 import re
 
-_, info = (
-    ffmpeg
-    .input('test-data/short.mp4')
-    .filter('select', 'gt(scene, 0.4)')
-    .filter('showinfo')
-    .output('pipe:', format='null')
-    .run(capture_stderr=True, quiet=True)
-)
+def get_scene_info(filename):
+    _, info = (
+        ffmpeg
+        .input(filename)
+        .filter('select', 'gt(scene, 0.4)')
+        .filter('showinfo')
+        .output('pipe:', format='null')
+        .run(capture_stderr=True, quiet=True)
+    )
 
-info = re.findall(r'pts_time:[0-9.]*', str(info))
-info = [float(x.split(':')[1]) for x in info]
-info.insert(0, 0.0)
+    info = re.findall(r'pts_time:[0-9.]*', str(info))
+    info = [float(x.split(':')[1]) for x in info]
+    info.insert(0, 0.0)
 
-print(info)
+    return info
+
+def split_scene(filename, out_dir):
+    info = get_scene_info(filename)
+    for i in range(len(info) - 1):
+        start = info[i]
+        end = info[i + 1]
+        out_filename = '{}/{:06d}.mp4'.format(out_dir, i)
+        print('Splitting {} from {} to {}'.format(filename, start, end))
+        (
+            ffmpeg
+            .input(filename, ss=start, to=end)
+            .output(out_filename)
+            .run()
+        )
+
+split_scene('./test-data/short.mp4', './test-data/split/short')
