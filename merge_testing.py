441d97056bf94cd2276eac620ae7cbd9b0655a84
Adam <adam66ak47@gmail.com>
2021-08-26 22:25:00 +0200

    better CLI formatting

--- merge_testing.py
+++ merge_testing.py
@@ -6,18 +6,18 @@ from merge_video import merge_video, get_cut_list
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Merge video and audio files into a music video', usage='%(prog)s video_path audio_path [options]')
+    parser = argparse.ArgumentParser(description='Merge video and audio files into a music video', usage='%(prog)s video_path audio_path [options]', formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=32))
     parser.add_argument('video_path', help='video path (.mp4, .avi)')
     parser.add_argument('audio_path', help='audio path (.mp3, .wav)')
-    parser.add_argument('-o', '--output-path', help='output path', default='./merged.mp4')
+    parser.add_argument('-o', '--output-path', help='output path', default='./merged.mp4', metavar='PATH')
     parser.add_argument('--method', choices=['delay', 'pseudochrono', 'random'], default='delay', help='clip arrangement method')
 
     detector_options = parser.add_argument_group('adaptive scene detector')
-    detector_options.add_argument('--threshold', help='threshold for adaptive scene detection', default=3.0, type=float)
-    detector_options.add_argument('--luma-only', action='store_true', help='use only luma values in scene detection')
-    detector_options.add_argument('--min-scene-len', help='minimum length of scene in frames', default=15, type=int)
-    detector_options.add_argument('--min-delta-hsv', help='minimum delta between scene frames in HSV color space', default=15.0, type=float)
-    detector_options.add_argument('--window-width', help='width of rolling average window for scene detection', default=2, type=int)
+    detector_options.add_argument('--threshold', help='threshold value that the calculated frame score must exceed to trigger a new scene', default=3.0, type=float, metavar='FLOAT')
+    detector_options.add_argument('--min-scene-len', help='minimum length of scene in frames', default=15, type=int, metavar='INT')
+    detector_options.add_argument('--min-delta-hsv', help='minimum delta between scene frames in HSV color space', default=15.0, type=float, metavar='FLOAT')
+    detector_options.add_argument('--window-width', help='width of rolling average window for scene detection', default=2, type=int, metavar='INT')
+    detector_options.add_argument('--luma-only', action='store_true', help='only consider luma/brightness channel')
 
     args = parser.parse_args()
     
